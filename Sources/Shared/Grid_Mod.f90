d904e6c27dfb40b3dab6579d32309a3e253280d7
filippo-moroni <121792527+filippo-moroni@users.noreply.github.com>
2023-01-17 17:43:42 +0100

    Update Grid_Mod.f90

--- Sources/Shared/Grid_Mod.f90
+++ Sources/Shared/Grid_Mod.f90
@@ -34,7 +34,7 @@
 
     ! Stores the name of this domain
     character(SL) :: name
-    logical       :: polyhedral = .false.
+    logical       :: polyhedral
 
     ! Number of ...
     integer :: n_nodes     = 0  ! ... nodes
@@ -219,6 +219,7 @@
       procedure :: Save_Cfn
       procedure :: Save_Dim
       procedure :: Save_Debug_Vtu
+      procedure :: Save_Inertia        
       procedure :: Save_Vtu_Cells
       procedure :: Save_Vtu_Edges
       procedure :: Save_Vtu_Faces
@@ -270,6 +271,7 @@
 #   include "Grid_Mod/Save_Cfn.f90"
 #   include "Grid_Mod/Save_Dim.f90"
 #   include "Grid_Mod/Save_Debug_Vtu.f90"
+#   include "Grid_Mod/Save_Inertia.f90"          
 #   include "Grid_Mod/Save_Vtu_Cells.f90"
 #   include "Grid_Mod/Save_Vtu_Edges.f90"
 #   include "Grid_Mod/Save_Vtu_Faces.f90"
