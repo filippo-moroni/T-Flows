ca120a2eafb39011e30774011b72a4b58f594968
filippo-moroni <121792527+filippo-moroni@users.noreply.github.com>
2023-03-14 10:37:09 +0100

    Update Save_Wall_Properties.f90

--- Sources/Shared/Grid_Mod/Save_Wall_Properties.f90
+++ Sources/Shared/Grid_Mod/Save_Wall_Properties.f90
@@ -11,54 +11,46 @@
     
   type Snapshot
 
-  	real, allocatable :: x_snap(:), y_snap(:), z_snap(:)     ! Coordinates
-	real, allocatable :: nx_snap(:), ny_snap(:), nz_snap(:)  ! Surface vector components
-        real, allocatable :: ds_snap(:)                          ! Surface vector magnitude
-        real, allocatable :: wd_snap(:)                          ! Wall distance
+  	real, allocatable    :: x_snap(:), y_snap(:), z_snap(:)     ! Coordinates
+	real, allocatable    :: nx_snap(:), ny_snap(:), nz_snap(:)  ! Surface vector components
+        real, allocatable    :: ds_snap(:)                          ! Surface vector magnitude
+        real, allocatable    :: wd_snap(:)                          ! Wall distance
 
-	real, allocatable :: x_2(:), y_2(:), z_2(:)
+	real, allocatable    :: x_2(:), y_2(:), z_2(:)              ! Coordinates for the sorting
 
-	integer, allocatable :: pos_ind(:)
+	integer, allocatable :: pos_ind(:)                          ! Position index
 
   end type
 
   type(Snapshot)  :: Snap
   
     real          :: nx,ny,nz  ! Normal vector components (versor).
-    real          :: ds        ! Surface area of the element.
-      
+    real          :: ds        ! Surface area of the element.     
     integer       :: s,c1,c2    
     integer       :: l = 0     ! Integer to count how many cells we have at the selected boundary.
-    integer       :: c
-     
+    integer       :: c, iunit   
     character(SL) :: answer            
 !==============================================================================!
 
-  ! Display the BCs and save the BC's properties we are going to save 
-  
+  ! Display the BCs list and save the selected BC's properties 
   call Print_Bnd_Cond_List(Grid)
- 
   print '(a)', ' #==================================='
   print '(a)', ' # Insert the BC name in upper cases '
-  print '(a)', ' #-----------------------------------'
-   
+  print '(a)', ' #-----------------------------------' 
   read *, answer
     
   ! Creation of a .txt file containing the cells' coordinates and their properties
-
-  open (unit=1, file='Wall_prop_uns.txt', form='formatted', status='unknown')
+  open (newunit=iunit, file='Wall_prop_uns.txt', form='formatted', status='unknown')
    
   do s = 1, Grid % n_faces
   
   	c1 = Grid % faces_c(1,s)
   	c2 = Grid % faces_c(2,s)
   	
-  	! Take only faces at the boundaries
-  	
+  	! Take only faces at the boundaries  	
   	if(c2 < 0) then
   	
-  		! Selection of the boundary we asked before
-  		
+  		! Selection of the boundary we asked before 		
   		if(Grid % Bnd_Cond_Name(c2) .eq. answer) then
   		
   		ds = sqrt((Grid % sx(s))**2 + (Grid % sy(s))**2 + (Grid % sz(s))**2)
@@ -67,25 +59,24 @@
   		ny = Grid % sy(s) / ds
   		nz = Grid % sz(s) / ds
   	 	  	
-		write (1, *) Grid % xc(c1),       &  ! x-coordinate
-                     	     Grid % yc(c1),       &  ! y-coordinate
-                    	     Grid % zc(c1),       &  ! z-coordinate                   
-			     nx,                  &  ! x-component normal vector
-			     ny,                  &  ! y-component normal vector
-			     nz,                  &  ! z-component normal vector			     
-                     	     ds,                  &  ! ds surface area
-                    	     Grid % wall_dist(c1)    ! wall distance
+		write (iunit, *) Grid % xc(c1),       &  ! x-coordinate
+                     	         Grid % yc(c1),       &  ! y-coordinate
+                    	         Grid % zc(c1),       &  ! z-coordinate                   
+			         nx,                  &  ! x-component normal vector
+			         ny,                  &  ! y-component normal vector
+			         nz,                  &  ! z-component normal vector			     
+                     	         ds,                  &  ! ds surface area
+                    	         Grid % wall_dist(c1)    ! wall distance
                      
-                             l = l + 1               ! Parameter to count...
-                                                     ! ... how many cells we have.
-        
+                                 l = l + 1               ! Parameter to count...
+                                                         ! ... how many cells we have.     
   		end if
   	
   	end if
      
   end do
   
-  close (1)
+  close (iunit)
 
   ! Opening of the .txt file created to store the variables in the Snapshot
 
@@ -98,11 +89,11 @@
   allocate(Snap % ds_snap(l));   Snap % ds_snap = 0.0
   allocate(Snap % wd_snap(l));   Snap % wd_snap = 0.0
  
-  open (unit=2, file='Wall_prop_uns.txt', form='formatted', status='unknown')
+  open (newunit=iunit, file='Wall_prop_uns.txt', form='formatted', status='unknown')
    
   do c = 1, l
                
-  	read (2,*)          Snap % x_snap(c),   &
+  	read (iunit,*)      Snap % x_snap(c),   &
                             Snap % y_snap(c),   &
                             Snap % z_snap(c),   &
                             Snap % nx_snap(c),  &
@@ -114,7 +105,7 @@
                                                                                                    			       
   end do
   
-  close (2, status='delete')
+  close (iunit, status='delete')
   	
   ! Ordering the cells through their coordinates
 
@@ -139,22 +130,22 @@
 
   ! Writing the file with ordered cells according to their coordinates
 
-  open(unit=3,file='Wall_Properties.txt',form='formatted',status='unknown')
+  open(newunit=iunit,file='Wall_Properties.txt',form='formatted',status='unknown')
 
   do c = 1, l
 
-	write(3, *) Snap % x_snap(Snap % pos_ind(c)), ',', &
-		    Snap % y_snap(Snap % pos_ind(c)), ',', &
-		    Snap % z_snap(Snap % pos_ind(c)), ',', &
-		    Snap % nx_snap(Snap % pos_ind(c)), ',', &
-		    Snap % ny_snap(Snap % pos_ind(c)), ',', &
-		    Snap % nz_snap(Snap % pos_ind(c)), ',', &
-		    Snap % ds_snap(Snap % pos_ind(c)), ',', &
-		    Snap % wd_snap(Snap % pos_ind(c))
+	write(iunit, *) Snap % x_snap(Snap % pos_ind(c)), ',', &
+		        Snap % y_snap(Snap % pos_ind(c)), ',', &
+		        Snap % z_snap(Snap % pos_ind(c)), ',', &
+		        Snap % nx_snap(Snap % pos_ind(c)), ',', &
+		        Snap % ny_snap(Snap % pos_ind(c)), ',', &
+		        Snap % nz_snap(Snap % pos_ind(c)), ',', &
+		        Snap % ds_snap(Snap % pos_ind(c)), ',', &
+		        Snap % wd_snap(Snap % pos_ind(c))
 
   end do 
   
-  close(3)
+  close(iunit)
 
   deallocate(Snap % x_snap)     
   deallocate(Snap % y_snap)     
